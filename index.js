3d24f5c71255ee03b497b8e1e4b1d69d940d678c
rodennis <rev.rltodd@gmail.com>
2021-09-18 19:52:18 -0500

    was able to get player input to change grid size

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const container = document.querySelector("#container");
+const gridSize = prompt("Choose your grid size. (16 - 100")
 
-for (let i = 0; i < 17; i++) {
+for (let i = 0; i < gridSize; i++) {
     const div = document.createElement("div");
     div.classList.add("sketch")
     container.appendChild(div);
     div.addEventListener("mouseover", function( event ) {
         event.target.style.cssText = "background-color: black";
 })
-
 }
+
 
   
