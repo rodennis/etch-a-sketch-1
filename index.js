1d76a7fd74d55bef53d933c79c83829691ba78cd
rodennis <rev.rltodd@gmail.com>
2021-09-18 21:43:02 -0500

    working on changing the size of grid with players input

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const container = document.querySelector("#container");
-const gridSize = prompt("Choose your grid size. (16 - 100")
 
-for (let i = 0; i < gridSize; i++) {
+const grid = prompt("Choose your grid size. (16 - 100")
+
+function gridSize()
+    if (grid <= 100) {
+        container.style.cssText = "grid-template-columns: repeat(" + grid + ", 2fr)"
+        container.style.cssText = "grid-template-rows: repeat(" + grid + ", 2fr)";
+    }
+
+    /** working on button and text input  */
+/**const gridSize = document.querySelector("#gridsize");
+
+const submit = document.querySelector("#submit");
+submit.addEventListener('click', () => {
+
+  });**/
+
+for (let i = 0; i < grid ** 2; i++) {
     const div = document.createElement("div");
     div.classList.add("sketch")
     container.appendChild(div);
@@ -9,6 +24,4 @@ for (let i = 0; i < gridSize; i++) {
         event.target.style.cssText = "background-color: black";
 })
 }
-
-
   
